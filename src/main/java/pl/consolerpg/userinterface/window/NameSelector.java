eda3317119bc28a3af5649854a34f7aaca439727
KarwszPL <karwszpl@gmail.com>
2022-06-12 02:21:17 +0200

    Added rows, flexibility changes

--- src/main/java/pl/consolerpg/userinterface/window/NameSelector.java
+++ src/main/java/pl/consolerpg/userinterface/window/NameSelector.java
@@ -18,13 +18,17 @@ public class NameSelector extends UIWindow {
 
     private int nameIndex = 0;
 
-    private static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+
+
+
+    private static final String[] letters = new String[]{
+            "qwertyuiop",
+            "asdfghjkl",
+            "zxcvbnm"};
 
     private final int xStart = 2;
     private final int yStart = 5;
-
-    private final int xLimit = xStart + alphabet.length() - 1;
-    private final int yLimit = yStart + 2;
 
     private int selectedX = xStart;
     private int selectedY = yStart;
@@ -36,7 +40,7 @@ public class NameSelector extends UIWindow {
     }
 
     private void setLetter(String letter) {
-        display[nameStartY][nameStartX + nameIndex] = letter;
+        display[nameStartY][nameStartX + nameIndex + 1] = letter;
         nameIndex++;
     }
 
@@ -50,6 +54,9 @@ public class NameSelector extends UIWindow {
 
         display = initialDisplay();
 
+
+
+        //Leters
         sections.add(new Section() {
 
 
@@ -66,11 +73,25 @@ public class NameSelector extends UIWindow {
 
             private void move(int x, int y) {
                 display[selectedY][selectedX] = " ";
-                if (x >= xStart && x <= xLimit) selectedX = x;
-                if (y >= yStart && y <= yLimit) selectedY = y;
+                if (x < xStart) selectedX = getRowLength(selectedY);
+                else if (!isXInsideLimit(x)) selectedX = xStart;
+                else selectedX = x;
+                //if (x >= xStart && isXInsideLimit(x)) selectedX = x;
+                if (y >= yStart && isYInsideLimit(y)) selectedY = y;
                 display[selectedY][selectedX] = "^";
             }
 
+            private boolean isYInsideLimit(int y) {
+                return y - yStart < letters.length * 2;
+            }
+
+            private boolean isXInsideLimit(int x) {
+                return x - xStart < letters[getRow(selectedY)].length();
+            }
+
+            private boolean isXInsideLimit(int x, int y) {
+                return x - xStart < letters[getRow(y)].length();
+            }
 
 
             @Override
@@ -82,10 +103,22 @@ public class NameSelector extends UIWindow {
                     move(selectedX - 1, selectedY);
                 }
                 else if (event.getKeyCode() == UserInterface.SHIFT) {
-                    UIUtils.insertStringAtIndex(display, 2, 4, alphabet.toUpperCase(Locale.ROOT));
+                    for (int i = 0; i < letters.length; i++) {
+                        UIUtils.insertStringAtIndex(display, 2, 4 + i * 2, letters[i].toUpperCase(Locale.ROOT));
+                    }
                 }
                 else if (event.getKeyCode() == KeyEvent.VK_DOWN) {
-                    selectSection(selectedX > alphabet.length() / 2 ? 2 : 1);
+                    int newY = selectedY + 2;
+                    if (isYInsideLimit(newY)) {
+                        if (!isXInsideLimit(selectedX, newY)) {
+                            move(getRowLength(newY), newY);
+                        }
+                        else move(selectedX, newY);
+                    }
+                    else selectSection(selectedX > letters[0].length() / 2 ? 2 : 1);
+                }
+                else if (event.getKeyCode() == KeyEvent.VK_UP) {
+                    if (selectedY > yStart) move(selectedX, selectedY - 2);
                 }
                 else if (event.getKeyCode() == KeyEvent.VK_ENTER) {
                     setLetter();
@@ -95,22 +128,26 @@ public class NameSelector extends UIWindow {
             @Override
             public void onKeyRelease(KeyEvent event) {
                 if (event.getKeyCode() == UserInterface.SHIFT) {
-                    UIUtils.insertStringAtIndex(display, 2, 4, alphabet.toLowerCase(Locale.ROOT));
+                    for (int i = 0; i < letters.length; i++) {
+                        UIUtils.insertStringAtIndex(display, 2, 4 + i * 2, letters[i].toLowerCase(Locale.ROOT));
+                    }
                 }
             }
         });
 
+
+        //Space
         sections.add(new Section() {
 
 
             @Override
             public void select() {
-                display[yStart + 2][xStart] = "^";
+                display[11][xStart] = "^";
             }
 
             @Override
             public void deselect() {
-                display[yStart + 2][xStart] = " ";
+                display[11][xStart] = " ";
             }
 
             @Override
@@ -130,23 +167,25 @@ public class NameSelector extends UIWindow {
 
         });
 
+
+        //Backspace
         sections.add(new Section() {
 
 
             @Override
             public void select() {
-                display[yStart + 2][xStart + 17] = "^";
+                display[11][xStart + 6] = "^";
             }
 
             @Override
             public void deselect() {
-                display[yStart + 2][xStart + 17] = " ";
+                display[11][xStart + 6] = " ";
             }
 
             @Override
             public void onKeyPress(KeyEvent event) {
                 if (event.getKeyCode() == KeyEvent.VK_UP) {
-                    selectedX = xStart + 17;
+                    selectedX = xStart + 6;
                     selectSection(0);
                 }
                 else if (event.getKeyCode() == KeyEvent.VK_LEFT) {
@@ -168,19 +207,27 @@ public class NameSelector extends UIWindow {
     }
 
 
+    private int getRow(int y) {
+        return (y - yStart) / 2;
+    }
+
+    private int getRowLength(int y) {
+        return letters[getRow(y)].length() + 1;
+    }
+
     private static String [][]initialDisplay() {
         return UIUtils.stringToTable(
                         "==================================================\n" +
                         "|                                                |\n" +
                         "|  YOUR NAME =                                   |\n" +
                         "|                                                |\n" +
-                        "| abcdefghijklmnopqrstuvwxyz                     |\n" +
+                        "| qwertyuiop                                     |\n" +
                         "|                                                |\n" +
-                        "| SPACE            BACKSPACE                     |\n" +
+                        "| asdfghjkl                                      |\n" +
                         "|                                                |\n" +
+                        "| zxcvbnm                                        |\n" +
                         "|                                                |\n" +
-                        "|                                                |\n" +
-                        "|                                                |\n" +
+                        "| Space Backspace                                |\n" +
                         "|                                                |\n" +
                         "|                                                |\n" +
                         "|                                                |\n" +
